9122de62c3a389f18fa1f3b774a17b09ada7f743
Forkan98 <forkanmohammad206@gmail.com>
2022-01-08 15:10:46 -0330

    update upto 2021

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,22 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAiuVis8oiSEgHFJGLGJDsWH3stHwnYLJ8",
+    authDomain: "ema-website-firebase.firebaseapp.com",
+    projectId: "ema-website-firebase",
+    storageBucket: "ema-website-firebase.appspot.com",
+    messagingSenderId: "767365231624",
+    appId: "1:767365231624:web:8686b424a9eae429520dbd"
+};
+
+export default firebaseConfig;
+
+// console.log(process.env);
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID,
+// };
+
+// export default firebaseConfig;
